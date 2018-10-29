dd55d3707c87db5c69f0690569429897ec81a7f8
Brian <briandsalemi@gmail.com>
2018-10-29 15:10:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector);
+}
 
+function nestedTarget() {
+ return document.getElementById(`nested`).querySelector(`.target`);
+}
+
+function increaseRankBy(n) {
+  const lists = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lists.length; i++) {
+  lists[i].innerHTML= parseInt(lists[i].innerHTML) + n;
+} }
+
+function deepestChild() {
+ var elem = document.getElementById(`grand-node`);
+  while (elem.children.length === 0) {
+  elem = elem.children[0];
+  }
+  return elem;
+}
